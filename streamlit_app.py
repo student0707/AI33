8f459f6d676d828e04bd13bd83a83aea429a3f09
student0707 <20242817@sdhs.gwe.hs.kr>
2024-12-13 23:55:48 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,16 @@ import streamlit as st
 import pandas as pd
 
 # 1. 제목
-st.title("Streamlit 기본 예제 페이지")
+st.title("이소은의 스트림릿 서비스")
 
 # 2. 부제목
-st.subheader("이 페이지는 다양한 Streamlit 기능을 보여줍니다.")
+st.subheader("암세포와 정상세포 이미지 분류하기")
 
 # 3. 판다스 데이터프레임 기반 표 출력
 df = pd.DataFrame({
-    "Name": ["Alice", "Bob", "Charlie"],
-    "Age": [24, 30, 29],
-    "Country": ["Korea", "USA", "UK"]
+    "이름": ["이소은", "이강인 ", "김은진"],
+    "나이": [18, 22, 18],
+    "나라": ["Korea", "USA", "UK"]
 })
 st.write("데이터프레임 예제")
 st.dataframe(df)
@@ -36,7 +36,7 @@ st.markdown(
     """
     <style>
     .styled-box {
-        padding: 10px;
+        padding: 20px;
         margin: 5px;
         background-color: lightgreen;
         border-radius: 5px;
@@ -52,7 +52,7 @@ st.markdown(
 
 # 6. 이미지 표시
 st.write("이미지 표시 예제")
-st.image("https://www.streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png", caption="Streamlit 로고")
+st.image("https://img.khan.co.kr/news/2024/03/23/news-p.v1.20240323.c159a4cab6f64473adf462d873e01e43_P1.jpg")
 
 # 7. 유튜브 링크 (썸네일 표시)
 st.write("유튜브 동영상 예제")
